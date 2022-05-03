74356a4385ae0b108637ecd2e7630f748a5dab6e
Andrey Nelyubov <anelyubov@dxc.com>
2022-05-03 22:06:43 +1200

    4

--- widget.js
+++ widget.js
@@ -18,65 +18,64 @@ const B52AreaHtml = `
 </div>
 `;
 
-function B52Widget(B52Tv, BinanceAdapter,theme)
-{
-    this.tv = B52Tv;
-    this.theme = theme;
-    this.b = BinanceAdapter;
-}
+class B52Widget {
+    constructor(B52Tv, BinanceAdapter, theme) {
+        this.tv = B52Tv;
+        this.theme = theme;
+        this.b = BinanceAdapter;
+    }
+    Build() {
+        $('body').append('<div id="B52Area" draggable="true" class="' + this.theme + '"></div>');
+        $('#B52Area').html(B52AreaHtml);
 
-B52Widget.prototype.Build = function()
-{
-    $('body').append('<div id="B52Area" draggable="true" class="'+this.theme+'"></div>');
-    $('#B52Area').html(B52AreaHtml);
-
-    //events
-    $("#B52ClearChart").click(() => {
-        this.tv.learB52s();
-      });
-      $("#B52Start100").click(() => {
-        this.tv.runFavIndicator($("#B52Start100").text());
-      });
-      $("#B52StartBinance").click(() => {
-        var theUniqueName = "B52 " + Date.now().toString();
-        var menu = this.tv.xpathGetFirstItem("//div[@data-role='button' and @data-name='alerts']");
-        this.tv.triggerMouseEvent(menu, "click");
-        this.tv.createNewAlert(theUniqueName, () => {
-          this.tv.grabAlertMessage(theUniqueName, (res) => {
-            $("#B52Result").text(res);
-            setTimeout(function() {
-              deleteAlert(this.tv.getCurrentCurrencyPair(), theUniqueName);
-            }, 50);
-          });
+        //events
+        $("#B52ClearChart").click(() => {
+            this.tv.learB52s();
         });
-      });
-      $("#B52ConnectBinance").click(() => {
-        this.b.binanceGetTickSize((size)=>{$("#B52ConnectionStatus").text(size.toString());});
-      });
-      this.Stlye();
+        $("#B52Start100").click(() => {
+            this.tv.runFavIndicator($("#B52Start100").text());
+        });
+        $("#B52StartBinance").click(() => {
+            var theUniqueName = "B52 " + Date.now().toString();
+            var menu = this.tv.xpathGetFirstItem("//div[@data-role='button' and @data-name='alerts']");
+            this.tv.triggerMouseEvent(menu, "click");
+            this.tv.createNewAlert(theUniqueName, () => {
+                this.tv.grabAlertMessage(theUniqueName, (res) => {
+                    $("#B52Result").text(res);
+                    setTimeout(function () {
+                        deleteAlert(this.tv.getCurrentCurrencyPair(), theUniqueName);
+                    }, 50);
+                });
+            });
+        });
+        $("#B52ConnectBinance").click(() => {
+            this.b.binanceGetTickSize((size) => { $("#B52ConnectionStatus").text(size.toString()); });
+        });
+        this.Stlye();
+    }
+    Stlye() {
+        $("#B52Area.dark").css({
+            "-webkit-user-drag": "element",
+            "resize": "both",
+            "position": "absolute",
+            "right": "57px",
+            "bottom": "25px",
+            "border": "1px solid gray",
+            "height": "350px",
+            "width": "240px",
+            "background-color": "black",
+            "padding": "5px"
+        });
+
+        $("#B52Area.dark").find("div").css({
+            "padding": "2px"
+        });
+
+        $("#B52Area.dark").find("button").css({
+            "border": "1px solid gray",
+            "background-color": "#3f5721"
+        });
+    }
 }
 
-B52Widget.prototype.Stlye = function()
-{
-    $("#B52Area.dark").css({
-        "-webkit-user-drag": "element",
-        "resize": "both",
-        "position": "absolute",
-        "right": "57px",
-        "bottom": "25px",
-        "border": "1px solid gray",
-        "height": "350px",
-        "width": "240px",
-        "background-color": "black",
-        "padding": "5px"
-    });
-  
-  $("#B52Area.dark").find("div").css({
-      "padding": "2px"
-  });
-  
-  $("#B52Area.dark").find("button").css({
-     "border": "1px solid gray",
-     "background-color": "#3f5721"
-  });
-}
+
