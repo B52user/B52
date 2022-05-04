475a46d08677b7044dc9439f37c9700d65783d2e
B52user <104760425+B52user@users.noreply.github.com>
2022-05-05 09:28:41 +1200

    Update b52.js

--- b52.js
+++ b52.js
@@ -116,3 +116,128 @@ class BinanceAdapter {
     }
 }
 
+const B52AreaHtml = `
+<div>
+  <button id='B52ClearChart'>Clear chart</button>
+</div>
+<div>
+  <button id='B52Start100'>B52</button>
+</div>
+<div>
+  <button id='B52StartBinance'>START!</button>
+</div>
+<div>
+  Binance connection status: <span id="B52ConnectionStatus"></span>
+  <button id='B52ConnectBinance'>Connect</button>
+</div>
+<div>
+  Results:
+  <div id='B52Result'></div>
+</div>
+`;
+
+class B52Widget {
+    constructor(B52Tv, BinanceAdapter, theme) {
+        this.tv = B52Tv;
+        this.theme = theme;
+        this.b = BinanceAdapter;
+    }
+    Build() {
+        $('body').append('<div id="B52Area" draggable="true" class="' + this.theme + '"></div>');
+        $('#B52Area').html(B52AreaHtml);
+
+        //events
+        $("#B52ClearChart").click(() => {
+            this.tv.clearB52s();
+        });
+        $("#B52Start100").click(() => {
+            this.tv.runFavIndicator($("#B52Start100").text());
+        });
+        $("#B52StartBinance").click(() => {
+            var theUniqueName = "B52 " + Date.now().toString();
+            var menu = this.tv.xpathGetFirstItem("//div[@data-role='button' and @data-name='alerts']");
+            this.tv.triggerMouseEvent(menu, "click");
+            var that = this;
+            this.tv.createNewAlert(theUniqueName, () => {
+                that.tv.grabAlertMessage(theUniqueName, (res) => {
+                    $("#B52Result").text(res);
+                    setTimeout(function () {
+                        that.tv.deleteAlert(that.tv.getCurrentCurrencyPair(), theUniqueName);
+                    }, 50);
+                });
+            });
+        });
+        $("#B52ConnectBinance").click(() => {
+            this.b.binanceGetTickSize((size) => { $("#B52ConnectionStatus").text(size.toString()); });
+        });
+        this.Stlye();
+    }
+
+    Stlye() {
+        $("#B52Area.dark").css({
+            "-webkit-user-drag": "element",
+            "resize": "both",
+            "position": "absolute",
+            "right": "57px",
+            "bottom": "25px",
+            "border": "1px solid gray",
+            "height": "350px",
+            "width": "240px",
+            "background-color": "black",
+            "padding": "5px"
+        });
+
+        $("#B52Area.dark").find("div").css({
+            "padding": "2px"
+        });
+
+        $("#B52Area.dark").find("button").css({
+            "border": "1px solid gray",
+            "background-color": "#3f5721"
+        });
+    }
+}
+
+class B52TvService
+{
+	AddAction(action)
+	{
+		this.arrayOfActions.push(action);
+	}
+	
+	Start() {
+		var that = this;
+		this.service = setInterval(function () {
+			for (var i = 0; i < that.arrayOfActions.length; i++) {
+ 			   that.arrayOfActions[i]();
+			}
+		}
+        , this.freq);
+	}
+	Stop() {
+		clearInterval(this.service);
+	}
+
+	constructor(freq){
+		this.arrayOfActions = [];
+		this.service = null;
+		this.freq = freq;
+	}
+}
+
+var tv = new B52Tv();
+var b = new BinanceAdapter(tv);
+var page = new B52Widget(tv,b,"dark");
+page.Build();
+var tvObserver = new B52TvService(100);
+tvObserver.AddAction(()=>{
+	//shit window 1
+	var shit1 = "//article[.//h2[text()='Unlock the full power of TradingView']]//button[starts-with(@class,'close-button')]";
+	if(tv.xpathItemCount(shit1)>0)
+	{
+		tv.triggerMouseEvent(tv.xpathGetFirstItem(shit1),"click");
+	}
+});
+tvObserver.Start();
+
+
