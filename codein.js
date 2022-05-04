3f7484fa9d68bf89a3e694a876acfd75f2ac2653
B52user <104760425+B52user@users.noreply.github.com>
2022-05-05 10:42:42 +1200

    Update codein.js

--- codein.js
+++ codein.js
@@ -1,10 +1,11 @@
 //div[starts-with(@class,'modal') and .//div[text()='No ads on any chart']]//button[@aria-label='Close']
+//article[starts-with(@class,'toast')]//button[starts-with(@class,'close-button')]
 
 tvObserver.AddAction(()=>{
-	//shit window 2
-	var shit2 = "//div[starts-with(@class,'modal') and .//div[text()='No ads on any chart']]//button[@aria-label='Close']";
-	if(tv.xpathItemCount(shit2)>0)
+	//shit window 3
+	var shit3 = "//article[starts-with(@class,'toast')]//button[starts-with(@class,'close-button')]";
+	if(tv.xpathItemCount(shit3)>0)
 	{
-		tv.triggerMouseEvent(tv.xpathGetFirstItem(shit2),"click");
+		tv.triggerMouseEvent(tv.xpathGetFirstItem(shit3),"click");
 	}
 });
