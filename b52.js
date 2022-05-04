4d858dd26a6c3b3bb847749fd6374b4967d15ed3
B52user <104760425+B52user@users.noreply.github.com>
2022-05-05 10:26:05 +1200

    Update b52.js

--- b52.js
+++ b52.js
@@ -238,6 +238,14 @@ tvObserver.AddAction(()=>{
 		tv.triggerMouseEvent(tv.xpathGetFirstItem(shit1),"click");
 	}
 });
+tvObserver.AddAction(()=>{
+	//shit window 2
+	var shit2 = "//div[starts-with(@class,'modal') and .//div[text()='No ads on any chart']]//button[@aria-label='Close']";
+	if(tv.xpathItemCount(shit2)>0)
+	{
+		tv.triggerMouseEvent(tv.xpathGetFirstItem(shit2),"click");
+	}
+});
 tvObserver.Start();
 
 
