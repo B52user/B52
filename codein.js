2984d44f81ad58e6fbae930009e8d85db73b5f44
B52user <104760425+B52user@users.noreply.github.com>
2022-05-05 10:00:31 +1200

    Update codein.js

--- codein.js
+++ codein.js
@@ -1,42 +1,10 @@
-class B52TvService
-{
-	AddAction(action)
-	{
-		this.arrayOfActions.push(action);
-	}
-	
-	Start() {
-		var that = this;
-		this.service = setInterval(function () {
-			for (var i = 0; i < that.arrayOfActions.length; i++) {
- 			   that.arrayOfActions[i]();
-			}
-		}
-        , this.freq);
-	}
-	Stop() {
-		clearInterval(this.service);
-	}
+//div[starts-with(@class,'modal') and .//div[text()='No ads on any chart']]//button[@aria-label='Close']
 
-	constructor(freq){
-		this.arrayOfActions = [];
-		this.service = null;
-		this.freq = freq;
-	}
-}
-
-var tv = new B52Tv();
-var b = new BinanceAdapter(tv);
-var page = new B52Widget(tv,b,"dark");
-page.Build();
-var tvObserver = new B52TvService(100);
 tvObserver.AddAction(()=>{
-	//shit window 1
-	var shit1 = "//article[.//h2[text()='Unlock the full power of TradingView']]//button[starts-with(@class,'close-button')]";
-	if(tv.xpathItemCount(shit1)>0)
+	//shit window 2
+	var shit2 = "//div[starts-with(@class,'modal') and .//div[text()='No ads on any chart']]//button[@aria-label='Close']";
+	if(tv.xpathItemCount(shit2)>0)
 	{
-		tv.triggerMouseEvent(tv.xpathGetFirstItem(shit1),"click");
+		tv.triggerMouseEvent(tv.xpathGetFirstItem(shit2),"click");
 	}
 });
-tvObserver.Start();
-
