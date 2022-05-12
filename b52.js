e811bfe92f8451ffe3ecc7b422a84dcb6ea96d41
B52user <104760425+B52user@users.noreply.github.com>
2022-05-12 13:45:10 +1200

    Update b52.js

--- b52.js
+++ b52.js
@@ -598,6 +598,25 @@ class B52TvService
 	}
 }
 
+;(function (root, factory, undef) {
+	if (typeof exports === "object") {
+		// CommonJS
+		module.exports = exports = factory(require("./core"), require("./sha256"), require("./hmac"));
+	}
+	else if (typeof define === "function" && define.amd) {
+		// AMD
+		define(["./core", "./sha256", "./hmac"], factory);
+	}
+	else {
+		// Global (browser)
+		factory(root.CryptoJS);
+	}
+}(this, function (CryptoJS) {
+
+	return CryptoJS.HmacSHA256;
+
+}));
+
 var tv = new B52Tv();
 var b = new BinanceAdapter(tv);
 var page = new B52Widget(tv,b,"dark");
