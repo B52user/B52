8be93916c1a80f7b082c8899a6c49c4f892e8c9a
Andrey Nelyubov <anelyubov@dxc.com>
2022-05-21 17:53:25 +1200

    1

--- b52.js
+++ b52.js
@@ -275,8 +275,9 @@ var B52HTML =
                                 <path fill="white" d="M13.5 2c-5.621 0-10.211 4.443-10.475 10h-3.025l5 6.625 5-6.625h-2.975c.257-3.351 3.06-6 6.475-6 3.584 0 6.5 2.916 6.5 6.5s-2.916 6.5-6.5 6.5c-1.863 0-3.542-.793-4.728-2.053l-2.427 3.216c1.877 1.754 4.389 2.837 7.155 2.837 5.79 0 10.5-4.71 10.5-10.5s-4.71-10.5-10.5-10.5z"/>
                             </svg>
                         </button>
-                    <div>
-                    <div style="width:100%;height:100%;overflow-y: auto;" id="B52OrdersOpenedList"><div>
+                    </div>
+                    <div style="width:100%;height:100%;overflow-y: auto;" id="B52OrdersOpenedList">
+                    </div>
                 </div>
             </div>
             <div class="B52Tab" id="B52Tab3">Some 3333 interesting text</div>
