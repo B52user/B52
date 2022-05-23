322181b48c975c25c85dc07569fe01654a5896a0
Andrey Nelyubov <anelyubov@dxc.com>
2022-05-23 20:33:47 +1200

    1

--- b52.js
+++ b52.js
@@ -16,7 +16,7 @@ var B52Settings =
     workbookEmptyCells:10,
     workbookDollars:true,
     workbookAutoScroll:120,
-    workBookDepth:50,
+    workBookDepth:500,
     workBookColorPerc:0.005,
 	sButtons : 
 	[
