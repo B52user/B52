e84b7604c91c931af7c76245511126e76f9c227b
Andrey Nelyubov <anelyubov@dxc.com>
2022-05-30 14:48:56 +1200

    1

--- b52.js
+++ b52.js
@@ -579,6 +579,8 @@ class B52 {
                             sets.push({label:"Current position in coins",value:currPos.positionAmt});
                             sets.push({label:"Entry price in $",value:currPos.entryPrice});
                         }
+                        console.log("Sending sets");
+                        console.log(sets);
                         if(sets.length) B52Tv.SetStrategySettings(sets);
                     });
                 });
